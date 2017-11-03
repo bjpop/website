7b36f5dec76a8242cde787a83402f4dad4c961e1
Bernie Pope <bjpope@unimelb.edu.au>
2017-11-03 12:37:23 +1100

    Added teaching page

--- website/main.py
+++ website/main.py
@@ -143,6 +143,10 @@ def render_pages(options, jinja_env):
     software_template.add_content("contents", options.templates, "software.yaml")
     software_template.render_page(jinja_env, options.outdir)
 
+    teaching_template = Template("teaching.html")
+    teaching_template.add_content("contents", options.templates, "teaching.yaml")
+    teaching_template.render_page(jinja_env, options.outdir)
+
 
 def identity(x):
     return x
